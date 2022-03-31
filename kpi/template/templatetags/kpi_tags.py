b8a11a126850d23fd14f9ed7435dfdae57c058f5
Francesco Filicetti <francesco.filicetti@unical.it>
2022-03-31 08:26:51 +0200

    fix: logout + translations

--- kpi/template/templatetags/kpi_tags.py
+++ kpi/template/templatetags/kpi_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 from django.utils import timezone
 
 
@@ -8,3 +9,10 @@ register = template.Library()
 @register.simple_tag
 def year_list():
     return range(2019, timezone.localdate().year+1)
+
+
+@register.simple_tag
+def settings_value(name, **kwargs):
+    value = getattr(settings, name, None)
+    if value and kwargs: return value.format(**kwargs)
+    return value
