830f6901e92b9c8bc6bd1cf1efe9cc0edfbae690
Francesco Filicetti <francesco.filicetti@unical.it>
2022-07-28 15:32:20 +0200

    fix: code to string

--- kpi/detection_management/models.py
+++ kpi/detection_management/models.py
@@ -13,7 +13,7 @@ class DetectionCode(models.Model):
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.code
+        return f'{self.code} - {self.description}'
 
 
 class Detection(CreatedModifiedBy, TimeStampedModel):
