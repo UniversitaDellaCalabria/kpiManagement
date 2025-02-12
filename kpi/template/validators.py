ca8f32211ff4ae4b63113bd762d3bac6a4022981
Francesco Filicetti <francesco.filicetti@unical.it>
2025-02-12 16:48:57 +0100

    fix: bug in validators

--- kpi/template/validators.py
+++ kpi/template/validators.py
@@ -10,31 +10,31 @@ from . settings import ATTACH_NAME_MAX_LEN, MAX_UPLOAD_SIZE, PERMITTED_UPLOAD_FI
 def validate_file_extension(f):
     if hasattr(f.file, "content_type"):
         content_type = f.file.content_type
-        if not content_type.lower() in settings.PERMITTED_UPLOAD_FILETYPE:
+        if not content_type.lower() in PERMITTED_UPLOAD_FILETYPE:
             raise ValidationError(
                 _('File extension not accepted: "{}". Enter {} only').format(
                     content_type,
-                    settings.PERMITTED_UPLOAD_FILETYPE,
+                    PERMITTED_UPLOAD_FILETYPE,
                 )
             )
 
 
 def validate_file_size(f):
-    if f.size > int(settings.MAX_UPLOAD_SIZE):
+    if f.size > int(MAX_UPLOAD_SIZE):
         raise ValidationError(
             _("File size too large: {} bytes. Max {} bytes").format(
                 f.size,
-                settings.MAX_UPLOAD_SIZE,
+                MAX_UPLOAD_SIZE,
             )
         )
 
 
 def validate_file_length(f):
     file_name = os.path.basename(f.name)
-    if len(file_name) > settings.ATTACH_NAME_MAX_LEN:
+    if len(file_name) > ATTACH_NAME_MAX_LEN:
         raise ValidationError(
             _("File name length too large: {} characters. Max {} characters").format(
                 len(file_name),
-                settings.ATTACH_NAME_MAX_LEN,
+                ATTACH_NAME_MAX_LEN,
             )
         )
